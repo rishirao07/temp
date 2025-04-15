e6e16c2733229bb12da86bd5beaf9c11ed495895
rishirao07 <rishirao2310@gmail.com>
2025-04-15 09:31:46 +0530

    added

---
+++ calc.js
@@ -0,0 +1,7 @@
+export function greet(){
+    return "Hello World"
+}
+
+export function add(a,b){
+    return a+b
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+// import {greet} from "./calc.js"
+// let result = greet()
+// console.log(result);
+
+import {add} from "./calc.js"
+let result = add(4,5)
+console.log(result);
+
